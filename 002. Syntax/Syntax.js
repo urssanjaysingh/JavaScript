bc23b17da57d6dd32c26971bdd08be437ba62104
Sanjay Singh <118108567+urssanjaysingh@users.noreply.github.com>
2023-02-25 07:22:40 +0530

    Add files via upload

---
+++ 002. Syntax/Syntax.js
@@ -0,0 +1,26 @@
+/*
+JavaScript can be implemented using JavaScript statements that are placed
+within the <script>... </script> HTML tags in a web page.
+You can place the <script> tags, containing your JavaScript, anywhere within
+you web page, but it is normally recommended that you should keep it within
+the <head> tags.
+The <script> tag alerts the browser program to start interpreting all the text
+between these tags as a script. A simple syntax of your JavaScript will appear as
+follows.
+
+<script ...>
+ JavaScript code
+</script>
+
+The script tag takes two important attributes:
+>> Language: This attribute specifies what scripting language you are using.
+Typically, its value will be javascript. Although recent versions of HTML
+(and XHTML, its successor) have phased out the use of this attribute.
+>> Type: This attribute is what is now recommended to indicate the scripting
+language in use and its value should be set to "text/javascript".
+So your JavaScript syntax will look as follows.
+
+<script language="javascript" type="text/javascript">
+ JavaScript code
+</script>
+*/
